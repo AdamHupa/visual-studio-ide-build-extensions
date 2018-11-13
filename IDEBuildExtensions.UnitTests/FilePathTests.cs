588392a1346b20f06061a29ca4c673e46632665a
ahupa-git <>
2018-11-13 20:33:13 +0100

    Add FilePath class and tests

---
+++ IDEBuildExtensions.UnitTests/FilePathTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using IDEBuildExtensions;
+
+namespace IDEBuildExtensions.UnitTests
+{
+    [TestClass]
+    public class FilePathTests
+    {
+        [TestMethod]
+        public void Validate_FilePath()
+        {
+            System.Collections.Generic.List<Tuple<bool, string>> testPathSet = new System.Collections.Generic.List<Tuple<bool, string>>()
+            {
+                new Tuple<bool, string>(true, @"obj\Debug\IDETestLibrary.dll.config"),
+                new Tuple<bool, string>(true, @"\\server\share\path\"),
+                new Tuple<bool, string>(true, @"\\server\share\path\file.txt"),
+                new Tuple<bool, string>(true, @"C:\Users\user_name\Desktop\Unified Work Solution\IDE Build Extensions\IDETestLibrary\obj\Debug\"),
+                new Tuple<bool, string>(true, @"C:\Users\user_name\Desktop\Unified Work Solution\IDE Build Extensions\IDETestLibrary\obj\Debug\IDETestLibrary.dll.config"),
+                
+                new Tuple<bool, string>(true,  @"C:\Users\user_name\Desktop\Regex File Path Validation\" + new string('s', 193) + @"\"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name\Desktop\Regex File Path Validation\" + new string('s', 193) + @"S_248plus\"),
+                new Tuple<bool, string>(true,  @"C:\Users\user_name\Desktop\Regex File Path Validation\1" + new string('s', 187) + @"S\aaaaaaaaaaaa.txt"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name\Desktop\Regex File Path Validation\1" + new string('s', 187) + @"S\aaaaaaaaaaaa_260plus.txt"),
+                new Tuple<bool, string>(true, @"C:\"),
+                new Tuple<bool, string>(true, @"\\?\Volume{26a21bda-a627-11d7-9931-806b9f6e6963}\"),
+                new Tuple<bool, string>(true, @"\\?\Volume{fda21bda-4277-a1d7-9abc-f06b9f6e6963}\"),
+                new Tuple<bool, string>(true, @"\\?\C:\Users\user_name\Desktop\"),
+                new Tuple<bool, string>(true, @"\\?\C:\Users\user_name\Desktop\notes.txt"),
+                
+                new Tuple<bool, string>(true,  @"C:\Users\user_name\Desktop\notes.txt"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name\\Desktop\notes.txt"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name\/Desktop\notes.txt"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name/\\Desktop\notes.txt"),
+                new Tuple<bool, string>(false, @"C:\Users\user_name\\Desktop\notes.txt"),
+
+                new Tuple<bool, string>(false, @"AUX"),
+                new Tuple<bool, string>(true,  @"AUXa"),
+                new Tuple<bool, string>(false, @"AUX\"),
+                new Tuple<bool, string>(true,  @"AUXa\"),
+                new Tuple<bool, string>(false, @"C:\AUX"),
+                new Tuple<bool, string>(true,  @"C:\AUXa"),
+                new Tuple<bool, string>(false, @"C:\AUX\"),
+                new Tuple<bool, string>(true,  @"C:\AUXa\"),
+                
+                new Tuple<bool, string>(false, @"C:\ path\path\"),
+                new Tuple<bool, string>(false, @"C:\.path\path\"),
+                new Tuple<bool, string>(false, @"C:\path \path\"),
+                new Tuple<bool, string>(false, @"C:\path.\path\"),
+
+                new Tuple<bool, string>(false, @"C:\ file"),
+                new Tuple<bool, string>(true,  @"C:\.file"),
+                new Tuple<bool, string>(false, @"C:\file "),
+                new Tuple<bool, string>(false, @"C:\file."),
+
+                //new Tuple<bool, string>(, @""),
+            };
+
+            foreach (var testEntry in testPathSet)
+            {
+                Assert.IsTrue(testEntry.Item1 == FilePath.Validate(testEntry.Item2),
+                              String.Format("Path validation should {0} for:\n{1}\n", (testEntry.Item1 ? "succeed" : "fail"), testEntry.Item2));
+            }
+        }
+    }
+}
---
+++ IDEBuildExtensions/FilePath.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // for: Directory, Path
+using System.Text.RegularExpressions; // for: Regex
+
+// link: https://docs.microsoft.com/en-gb/windows/desktop/FileIO/naming-a-file
+// link: https://en.wikipedia.org/wiki/Path_(computing)
+
+namespace IDEBuildExtensions
+{
+    // PathFileExistsW(path) - include "Shlwapi.h" and add reference: C:\Program Files (x86)\Windows Kits\8.1\Lib\winv6.3\um\x86\ShLwApi.lib
+
+    public static class FilePath
+    {
+        private static readonly char[] _invalidFileNameChars;
+        private static readonly char[] _invalidPathChars;
+
+        private static readonly Regex _invalidFileName =
+            new Regex(@"(^\s)|([\s.]$)|(^(AUX|COM[1-9]|CON|LPT[1-9]|NUL|PRN)$)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static readonly Regex _invalidRelativePath =
+            new Regex(@"((^|\\|/)[\s.])|(\\\\|//|\\/|/\\)|([\s.]($|\\|/))" +
+                      @"|(^|\\|/)(AUX|COM[1-9]|CON|LPT[1-9]|NUL|PRN)($|\\|/)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static readonly Regex _prefixLongNamingConvention =
+            new Regex(@"^(\\\\\?\\)", RegexOptions.Compiled);
+
+        private static readonly Regex _pathPrefix =
+            new Regex(@"^((\\\\\?\\)?[a-zA-Z]:\\)|(\\\\\?\\Volume\{[0-9a-fA-F]{8,}-[0-9a-fA-F]{4,}-[0-9a-fA-F]{4,}-[0-9a-fA-F]{4,}-[0-9a-fA-F]{12,}\}\\)|(\\\\)", RegexOptions.Compiled);
+
+
+        static FilePath()
+        {
+            // illegal characters: 0x01-0x1F + "\"*/:<>?\\|"
+
+            _invalidFileNameChars = Path.GetInvalidFileNameChars();
+            Array.Sort(_invalidFileNameChars);
+
+            var list = Path.GetInvalidPathChars().ToList();
+            list.AddRange("*:?");
+            list.Sort();
+            _invalidPathChars = list.ToArray(); // only excluding '/' and '\\'
+        }
+
+
+        public static bool Validate(string path)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+                return false;
+
+            bool longNamingConvention = _prefixLongNamingConvention.IsMatch(path);
+            if (longNamingConvention && path.Length > 32767)
+                return false;
+
+
+            Match match = _pathPrefix.Match(path);
+
+            int firstAfterPrefix = match.Length;
+            int lastBackslash = path.LastIndexOfAny(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar });
+
+
+            if (firstAfterPrefix > path.Length || firstAfterPrefix == lastBackslash + 2)
+                return false;
+
+            if (firstAfterPrefix == lastBackslash + 1)
+            {
+                if (firstAfterPrefix == path.Length)
+                    return true;
+
+                if (path.Length > 260)
+                    return false;
+
+                return ValidateFileName(path.Substring(lastBackslash + 1)); // validating file name
+            }
+
+            if (lastBackslash + 1 == path.Length)
+            {
+                /* this method assumes that all paths end with a backslash which is not a prerequisite in Universal Naming Convention
+                   so the allowed directory path string should be smaller by 1 character to accommodate the ending backslash */
+                if (path.Length > 247 + 1)
+                    return false;
+
+                return ValidateRelativePath(path.Substring(firstAfterPrefix, lastBackslash - firstAfterPrefix)); // validating path
+            }
+            else
+            {
+                if (path.Length > 260)
+                    return false;
+
+                return ValidateRelativePath(path.Substring(firstAfterPrefix, lastBackslash - firstAfterPrefix))
+                       && ValidateFileName(path.Substring(lastBackslash + 1)); // validating path and file name
+            }
+        }
+
+        public static bool ValidateFileName(string fileName)
+        {
+            if (String.IsNullOrWhiteSpace(fileName) || fileName.Split(_invalidFileNameChars).Length > 1)
+                return false;
+
+            if (_invalidFileName.IsMatch(fileName))
+                return false;
+
+            return true;
+        }
+
+        public static bool ValidateRelativePath(string relativePath)
+        {
+            if (String.IsNullOrWhiteSpace(relativePath) || relativePath.Split(_invalidPathChars).Length > 1)
+                return false;
+
+            if (_invalidRelativePath.IsMatch(relativePath))
+                return false;
+
+            return true;
+        }
+    }
+}
